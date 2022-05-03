6ec819227e039f41c88ea8552a8a38c160f135e2
GERLIN ORLANDO TORRES SAAVEDRA <gotorres@paradigmadigital.com>
2022-05-03 16:57:58 +0200

    Se programa un productor enviando un hola mundo al topico demo_java

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -1,7 +1,42 @@
 package io.conduktor.demos.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        log.info("Hello world");
+
+        // Crear las propiedades del productor
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Crear el productor
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //Crear un registro a producir
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+
+        // Enviar datos - asincronamente
+        producer.send(producerRecord);
+
+        /* Vaciar datos - sincronamente. Esto lo que quiere decir es,
+          bloquea esta linea de codigo hasta que kafka envie y reciba
+          todos los datos de mi productor.*/
+        producer.flush();
+
+        // vacia y cierra el productor, este motodo tambien llamada a flush() internamente.
+        producer.close();
     }
 }
