69b05fa6e31af619e6c22e080751713ea6559e99
Gerlin Orlando Torres Saavedra [MacBook-Pro-2019] <gtorresoft@gmail.com>
2022-06-24 23:17:25 +0200

    65. Wikimedia Producer Safe Producer Implementation

--- kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -20,6 +20,13 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // Serializador de la clave del mensaje a enviar
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // Serializador del valor del mensaje a enviar
 
+        // Configuracion de productor seguro (Cliente Kafka con version <=2.8)
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, Boolean.TRUE.toString()); // No permite que duplique mensajes publicados por el productor
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all"); // Esperamos confirmacion de escritura por kafka
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE)); // En caso de error la cantidad maxima de reintentos que se harian.
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, Integer.toString(5));
+        properties.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, Integer.toString(120000)); // Tiempo maximo que tiene el productor para publicar un mensaje en kafka despues de un error.
+
         // Crear el productor
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
