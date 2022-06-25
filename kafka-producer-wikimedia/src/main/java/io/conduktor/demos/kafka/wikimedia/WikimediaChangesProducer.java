1af29697948cd24707db8de0aaa1e5899aeff48a
Gerlin Orlando Torres Saavedra [MacBook-Pro-2019] <gtorresoft@gmail.com>
2022-06-25 19:25:06 +0200

    68. Wikimedia Producer High Throughput Implementation

--- kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -3,12 +3,23 @@ package io.conduktor.demos.kafka.wikimedia;
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.URI;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.COMPRESSION_TYPE_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.LINGER_MS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.BATCH_SIZE_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.RETRIES_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION;
+import static org.apache.kafka.clients.producer.ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG;
 
 public class WikimediaChangesProducer {
     public static void main(String[] args) throws InterruptedException {
@@ -16,16 +27,21 @@ public class WikimediaChangesProducer {
 
         // Crear las propiedades del productor
         Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); // IP y PUERTO del servidor de Kafka
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // Serializador de la clave del mensaje a enviar
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // Serializador del valor del mensaje a enviar
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); // IP y PUERTO del servidor de Kafka
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // Serializador de la clave del mensaje a enviar
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // Serializador del valor del mensaje a enviar
+
+        // Mejorar el rendimiento del procesamiento por lotes
+        properties.setProperty(COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(LINGER_MS_CONFIG, "20");
+        properties.setProperty(BATCH_SIZE_CONFIG, Integer.toString(32*1024));
 
         // Configuracion de productor seguro (Cliente Kafka con version <=2.8)
-        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, Boolean.TRUE.toString()); // No permite que duplique mensajes publicados por el productor
-        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all"); // Esperamos confirmacion de escritura por kafka
-        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE)); // En caso de error la cantidad maxima de reintentos que se harian.
-        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, Integer.toString(5));
-        properties.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, Integer.toString(120000)); // Tiempo maximo que tiene el productor para publicar un mensaje en kafka despues de un error.
+        properties.setProperty(ENABLE_IDEMPOTENCE_CONFIG, Boolean.TRUE.toString()); // No permite que duplique mensajes publicados por el productor
+        properties.setProperty(ACKS_CONFIG, "all"); // Esperamos confirmacion de escritura por kafka
+        properties.setProperty(RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE)); // En caso de error la cantidad maxima de reintentos que se harian.
+        properties.setProperty(MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, Integer.toString(5));
+        properties.setProperty(DELIVERY_TIMEOUT_MS_CONFIG, Integer.toString(120000)); // Tiempo maximo que tiene el productor para publicar un mensaje en kafka despues de un error.
 
         // Crear el productor
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
